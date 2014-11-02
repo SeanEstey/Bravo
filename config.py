36f0a7a93190380df819a8e0db0f4e4198c5a575
Sean Estey <estese@gmail.com>
2014-11-02 16:02:09 -0700

    interface

--- config.py
+++ config.py
@@ -3,8 +3,10 @@ import logging
 
 # TODO: Create a mongodb settings collection and webpage to manage these values
 
-FROM_NUMBER= '+17804138846'
-CALLER_ID= 'Winnifred Stewart Association'
+#FROM_NUMBER= '+17804138846'
+#CALLER_ID= 'Winnifred Stewart Association'
+FROM_NUMBER= '+118889689466'
+CALLER_ID= 'Empties to Winn'
 BROKER_URI= 'amqp://'
 URL= 'http://23.239.21.165:5000'
 COUNTER_URL='http://23.239.21.165/call/1'

2bea3ff36c49ccb6a81e043b51f73dc59d91b579
Sean Estey <estese@gmail.com>
2016-01-20 13:17:52 -0700

    folder structure, deleted unused files. added etw/wsa logo

--- html_templates.py
+++ html_templates.py
@@ -1,4 +1,38 @@
 
+email_frame = '''
+  <table style='max-width:600px; font-size:12pt;' border='1' cellspacing='0' cellpadding='0' align='center'>
+    <tr>
+      <td align='center' bgcolor='#ffffff' height='1'><img src='http://content.delivra.com/etapcontent//WinnifredStewartAssociation/Empties%20&amp;%20WINN%20Small.jpg' alt='' width='450' height='87' /></td>
+    </tr>
+    <tr>
+      <td>
+        <table cellpadding='25'>
+          <tr>
+            <td>
+              <table style='font-size:12pt; width: 100%;' border='0' cellspacing='0' cellpadding='10' align='center'>
+                !BODY!
+                  <tr>
+                    <td>
+                      <hr/>
+                    </td>
+                  </tr>
+                  <tr>
+                    <td style='text-align:center;'>
+                      <p><span style='font-size: 12pt;'>1-888-YOU-WINN</span></p>
+                      <p><a title='emptiestowinn' href='http://www.emptiestowinn.com'>www.emptiestowinn.com</a></p>
+                    </td>
+                  </tr>
+              </table>
+            </td>
+          </tr>
+        </table>
+      </td>
+    </tr>
+  </table>
+'''
+
+
+
 welcome_body = '''
 <table style='max-width:600px; font-size:12pt;' border='1' cellspacing='0' cellpadding='0' align='center'>
   <tbody>

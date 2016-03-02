6d13a68381dfa2d61218f66efec75e836cbefbb3
Sean Estey <estese@gmail.com>
2016-03-01 17:55:40 -0700

    Update etap.py

--- flask/etap.py
+++ flask/etap.py
@@ -3,6 +3,7 @@ import requests
 
 from config import *
 
+# Call PHP eTapestry script
 def call(func_name, keys, data):
   return json.loads(
     requests.post(ETAP_WRAPPER_URL, data=json.dumps({
@@ -11,3 +12,14 @@ def call(func_name, keys, data):
       "data": data
     })).text
   )
+
+# Extract User Defined Fields from eTap Account object. Allows
+# for UDF's which contain multiple fields (Block, Neighborhood)
+def get_udf(field_name, etap_account):
+  field_values = []
+
+  for field in etap_account['accountDefinedValues']:
+    if field['fieldName'] == field_name:
+      field_values.append(field['value'])
+
+  return ", ".join(field_values)

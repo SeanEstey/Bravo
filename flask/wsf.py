daefe9ba7e3c239114531d9ed899e1cdbc5d4d54
Sean Estey <estese@gmail.com>
2016-10-01 11:29:19 -0600

    Create wsf.py

---
+++ flask/wsf.py
@@ -0,0 +1,15 @@
+
+
+# Functions specific to WSF agency
+
+
+def resolve_depot(block, postal):
+    '''figure which depot
+    '''
+    
+    return True
+    
+def add_form_signup():
+
+
+    return True

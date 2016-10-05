6e9ec1306577c3140bb88bdb1bb7a4b42929dd38
Sean Estey <estese@gmail.com>
2016-10-05 12:23:40 -0600

    whoops

---
+++ app/routing/views.py
@@ -0,0 +1,108 @@
+# routing blueprint
+
+import json
+import twilio.twiml
+import requests
+from datetime import datetime, date, time, timedelta
+from flask import Blueprint, request, jsonify, render_template, redirect
+from flask.ext.login import login_required, current_user
+from bson.objectid import ObjectId
+import logging
+
+routing = Blueprint('routing', __name__, url_prefix='/routing')
+
+# Import modules and objects
+from app import db, app, socketio
+from app import utils
+from app import notific_events
+from app import pickup_service
+from app import notifications
+from app import notific_events
+from app import sms
+from app import tasks
+from app import routes
+
+# Get logger
+logger = logging.getLogger(__name__)
+
+#-------------------------------------------------------------------------------
+@routing.route('', methods=['GET'])
+@login_required
+def show_routing():
+    agency = db['users'].find_one({'user': current_user.username})['agency']
+    agency_conf = db['agencies'].find_one({'name':agency})
+    upcoming = routes.get_upcoming_routes(agency)
+
+    return render_template(
+      'views/routing.html',
+      routes=upcoming,
+      depots=agency_conf['routing']['depots'],
+      drivers=agency_conf['routing']['drivers']
+    )
+
+#-------------------------------------------------------------------------------
+@routing.route('/get_scheduled_route', methods=['POST'])
+def get_today_route():
+    return True
+    '''return json.dumps(get_scheduled_route(
+      etapestry_id['agency'],
+      request.form['block'],
+      request.form['date']))
+    '''
+
+#-------------------------------------------------------------------------------
+@routing.route('/get_route/<job_id>', methods=['GET'])
+def get_route(job_id):
+    agency = db['routes'].find_one({'job_id':job_id})['agency']
+    conf = db['agencies'].find_one({'name':agency})
+    api_key = conf['google']['geocode']['api_key']
+
+    return json.dumps(routes.get_orders(job_id, api_key))
+
+#-------------------------------------------------------------------------------
+@routing.route('/start_job', methods=['POST'])
+def get_routing_job_id():
+    logger.info('Routing Block %s...', request.form['block'])
+
+    etap_id = json.loads(request.form['etapestry_id'])
+
+    agency_config = db['agencies'].find_one({
+      'name':etap_id['agency']
+    })
+
+    try:
+        job_id = routes.submit_job(
+          request.form['block'],
+          request.form['driver'],
+          request.form['date'],
+          request.form['start_address'],
+          request.form['end_address'],
+          etap_id,
+          agency_config['routing']['routific']['api_key'],
+          min_per_stop=request.form['min_per_stop'],
+          shift_start=request.form['shift_start'])
+    except Exception as e:
+        logger.error(str(e))
+        return False
+
+    return job_id
+
+#-------------------------------------------------------------------------------
+@routing.route('/build/<route_id>', methods=['GET', 'POST'])
+def _build_route(route_id):
+    r = tasks.build_route.apply_async(
+      args=(route_id,),
+      queue=app.config['DB']
+    )
+
+    return redirect(app.config['PUB_URL'] + '/routing')
+
+#-------------------------------------------------------------------------------
+@routing.route('/build_sheet/<route_id>/<job_id>', methods=['GET'])
+def _build_sheet(job_id, route_id):
+    '''non-celery synchronous func for testing
+    '''
+    routes.build_route(route_id, job_id=job_id)
+    return redirect(app.config['PUB_URL'] + '/routing')
+
+

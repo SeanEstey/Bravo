52edb917a07f5bd470c3f3bcb1bccd88bc27907b
Sean Estey <estese@gmail.com>
2016-10-08 17:25:41 -0600

    Update run.py

--- run.py
+++ run.py
@@ -5,13 +5,7 @@ import getopt
 
 from app import app, socketio
 
-def restart_celery_worker():
-    os.system('kill %1')
-
-    # Kill celery nodes with matching queue name. Leave others alone
-    os.system("ps aux | grep 'queues " + app.config['DB'] + \
-              "' | awk '{print $2}' | xargs kill -9")
-
+def start_celery_worker():
     # Create workers
     os.system('celery worker -A app.celery_app -B -n ' + \
               app.config['DB'] + ' --queues ' + app.config['DB'] + ' &')
@@ -19,26 +13,35 @@ def restart_celery_worker():
     # Pause to give workers time to initialize before starting server
     time.sleep(3)
     
-    #if not celery_app.control.inspect().registered_tasks():
-    #    app.logger.info('Celery process failed to start!')
-    #else:
-    #    app.logger.info('Server starting using \'%s\' DB', app.config['DB'])
-    
+def restart_celery_worker():
+    os.system('kill %1')
+
+    # Kill celery nodes with matching queue name. Leave others alone
+    os.system("ps aux | grep 'queues " + app.config['DB'] + \
+              "' | awk '{print $2}' | xargs kill -9")
+
+    start_celery_worker()
 
 def main(argv):
-    '''Optionally restart celery worker with:
-        python run.py -r celery
+    '''
+    Start celery worker:
+        python run.py -c start
+    Restart celery worker:
+        python run.py -c restart
     '''
     
     try:
-        opts, args = getopt.getopt(argv,"r:")
+        opts, args = getopt.getopt(argv,"c:")
     except getopt.GetoptError:
         sys.exit(2)
     for opt, arg in opts:
-        if opt == '-r':
-            if arg == 'celery':
+        if opt == '-c':
+            if arg == 'restart':
                 print 'restarting celery worker'
                 restart_celery_worker()
+            elif arg == 'start':
+                print 'starting celery worker'
+                start_celery_worker()
 
     if app.config['DEBUG'] == True:
         # Werkzeug server (Test Mode)

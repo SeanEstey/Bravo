8685e3d0ed28f6d776eab42431aa88d7e71fb133
Sean Estey <estese@gmail.com>
2016-10-08 17:35:15 -0600

    Delete __eventlet.py

--- __eventlet.py
+++
@@ -1,7 +0,0 @@
-       wst = threading.Thread(target=self.serve_app, args=(sio,app))
-        wst.daemon = True
-        wst.start()
-
-def serve_app(self, _sio, _app):
-    app = socketio.Middleware(_sio, _app)
-    eventlet.wsgi.server(eventlet.listen(('', 7000)), app)

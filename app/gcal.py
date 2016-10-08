7c6c8bc9f469df1601ac3d9992d7da58a3d137e2
Sean Estey <estese@gmail.com>
2016-10-08 11:57:32 -0600

    Update gcal.py

--- app/gcal.py
+++ app/gcal.py
@@ -1,30 +1,35 @@
+import logging
+from oauth2client.client import SignedJwtAssertionCredentials
+import httplib2
+from apiclient.discovery import build
+import requests
+import json
+
+from app import app, db
+logger = logging.getLogger(__name__)
+
 
 #-------------------------------------------------------------------------------
 def gauth(oauth):
-    scope = [
-       ['https://www.googleapis.com/auth/calendar.readonly']
-    ]
-    version = 'v3'
-    name = 'calendar'
-
     try:
         credentials = SignedJwtAssertionCredentials(
             oauth['client_email'],
             oauth['private_key'],
-            scope
+            [
+                ['https://www.googleapis.com/auth/calendar.readonly']
+            ]
         )
 
         http = httplib2.Http()
         http = credentials.authorize(http)
-        service = build(name, version, http=http)
+        service = build('calendar', 'v3', http=http)
     except Exception as e:
         logger.error('Error authorizing %s: %s', name, str(e))
         return False
 
-    logger.debug('Drive service authorized')
+    logger.debug('Caelndar service authorized')
 
     return service
-
 
 #-------------------------------------------------------------------------------
 def get_events(service, cal_id, start, end, oauth):
@@ -36,20 +41,16 @@ def get_events(service, cal_id, start, end, oauth):
     Event object definition: https://developers.google.com/google-apps/calendar/v3/reference/events#resource
     '''
 
-    start = start.replace(tzinfo=None)
-    end = end.replace(tzinfo=None)
-
     try:
         events_result = service.events().list(
             calendarId = cal_id,
-            timeMin = start.isoformat() +'-07:00', # MST ofset
-            timeMax = end.isoformat() +'-07:00', # MST offset
+            timeMin = start.replace(tzinfo=None).isoformat() +'-07:00', # MST ofset
+            timeMax = end.replace(tzinfo=None).isoformat() +'-07:00', # MST offset
             singleEvents = True,
             orderBy = 'startTime'
         ).execute()
     except Exception as e:
         logger.error('Error pulling cal events: %s', str(e))
-        logger.error(start.isoformat())
         return False
 
     events = events_result.get('items', [])

93587c3adde978432290eb992bcb2aa19eed4c31
Sean Estey <estese@gmail.com>
2016-10-08 09:52:25 -0600

    Update non_participants.py

--- app/main/non_participants.py
+++ app/main/non_participants.py
@@ -10,12 +10,7 @@ def find(agency, accounts):
     # Build list of accounts to query gift_histories for
     viable_accounts = []
 
-    agency_settings = db['agencies'].find_one({'name':agency})
-
-    etap_id = agency_settings['etapestry']
-
-    keys = {'user':etap_id['user'], 'pw':etap_id['pw'],
-            'agency':agency,'endpoint':app.config['ETAPESTRY_ENDPOINT']}
+    agency_conf = db['agencies'].find_one({'name':agency})
 
     for account in accounts:
         # Test if Dropoff Date was at least 12 months ago
@@ -28,14 +23,17 @@ def find(agency, accounts):
 
             # If missing Signup Date or Dropoff Date, use 'accountCreatedDate'
             try:
-                etap.call('modify_account', keys, {
-                  'id': account['id'],
-                  'udf': {
-                    'Dropoff Date': date_str,
-                    'Signup Date': date_str
-                  },
-                  'persona': []
-                })
+                etap.call(
+                    'modify_account',
+                    agency_conf['etapestry'], 
+                    data={
+                        'id': account['id'],
+                        'udf': {
+                            'Dropoff Date': date_str,
+                            'Signup Date': date_str
+                        },
+                        'persona': []
+                    })
             except Exception as e:
                 logger.error('Error modifying account %s: %s', account['id'], str(e))
                 continue
@@ -58,13 +56,15 @@ def find(agency, accounts):
     logger.info('Non-participant cutoff date is %s', np_cutoff.strftime('%b %d %Y'))
 
     try:
-        # Retrieve non-zero gift donations from non-participant cutoff date to
-        # present
-        gift_histories = etap.call('get_gift_histories', keys, {
-          "account_refs": [i['ref'] for i in viable_accounts],
-          "start_date": str(np_cutoff.day) + "/" + str(np_cutoff.month) + "/" +str(np_cutoff.year),
-          "end_date": str(now.day) + "/" + str(now.month) + "/" + str(now.year)
-        })
+        # Retrieve non-zero gifts from cutoff date
+        gift_histories = etap.call(
+            'get_gift_histories',
+            keys, 
+            data = {
+                "account_refs": [i['ref'] for i in viable_accounts],
+                "start_date": str(np_cutoff.day) + "/" + str(np_cutoff.month) + "/" +str(np_cutoff.year),
+                "end_date": str(now.day) + "/" + str(now.month) + "/" + str(now.year)
+            })
     except Exception as e:
         logger.error('Failed to get gift_histories', exc_info=True)
         return str(e)

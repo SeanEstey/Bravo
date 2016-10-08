c61b805078fa4d5ef94d1ec7a834bf5594e91846
Sean Estey <estese@gmail.com>
2016-10-07 18:46:14 -0600

    Create mailgun.py

---
+++ app/mailgun.py
@@ -0,0 +1,29 @@
+# TODO: rename all "send_email" calls to "mailgun.send"
+
+
+#-------------------------------------------------------------------------------
+def send(to, subject, body, conf):
+    '''Send email via mailgun.
+    @conf: 'mailgun' dict from 'agencies' DB
+    Returns:
+      -mid string on success
+      -False on failure'''
+
+    try:
+        response = requests.post(
+          'https://api.mailgun.net/v3/' + conf['domain'] + '/messages',
+          auth=('api', conf['api_key']),
+          data={
+            'from': conf['from'],
+            'to':  to,
+            'subject': subject,
+            'html': body
+        })
+    except requests.RequestException as e:
+        logger.error('mailgun: %s ', str(e))
+        return False
+
+    logger.debug(response.text)
+
+    return json.loads(response.text)['id']
+

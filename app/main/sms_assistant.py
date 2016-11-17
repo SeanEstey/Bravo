e1a7630bc7ff9f09eac7ccae25a718e132ddbcc1
SeanEstey <estese@gmail.com>
2016-11-17 15:14:15 -0700

    sms assistant greeting

--- app/main/sms_assistant.py
+++ app/main/sms_assistant.py
@@ -45,58 +45,114 @@ def on_receive():
     '''
 
     logger.info(request.cookies)
-
     agency = db.agencies.find_one({'twilio.sms.number':request.form['To']})
-
-    resp = make_response()
-    twml = twiml.Response()
-
-    if not request.cookies.get('etap_id'):
-        account = identify_user(resp)
-
-        if not account:
-            twml.message(
-                "I'm sorry, I can't find an account linked to your phone number. "\
-                "Contact recycle@vecova.ca"
-            )
-            resp.data = str(twml)
-            return resp
-    else:
-        account = etap.call(
-          'get_account',
-          agency['etapestry'],
-          {'account_number':request.cookies['etap_id']},
-          silence_exceptions=True)
-
     msg = request.form['Body']
     from_ = request.form['From']
+    resp = make_response()
+    account = None
+    greeting = ''
 
+    expires=datetime.utcnow() + timedelta(hours=4)
+    resp.set_cookie(
+        'messagecount',
+        value=str(int(request.cookies.get('messagecount', 0))+1),
+        expires=expires.strftime('%a,%d %b %Y %H:%M:%S GMT'))
+
+    account = get_identity(resp)
+
+    if not account:
+        return send_reply(
+            "I'm sorry, I can't find an account linked to your phone number. "\
+            "Contact recycle@vecova.ca",
+            resp
+        )
+
+    if new_conversation():
+        greeting = get_greeting(account)
+
+    # Keyword handler
     if 'SCHEDULE' in msg.upper():
-        date = etap.get_udf('Next Pickup Date', account)
+        if not etap.get_udf('Next Pickup Date', account):
+            logger.error(
+                'missing Next Pickup Date for account %s (SMS: %s)',
+                str(account['id'], from_))
 
-        if not date:
-            logger.error('missing Next Pickup Date for account %s (SMS: %s)', str(account['id'], from_))
+            return send_reply(
+                "You are not currently scheduled for pickup. "\
+                "Please contact us recycle@vecova.ca",
+                resp
+            )
 
-            twml.message("You are not currently scheduled for pickup. Please contact us recycle@vecova.ca")
-        else:
-            twml.message('Your next pickup is ' + date)
-    elif 'PICKUP' in msg.upper():
-        pickup_request(resp, twml)
+        npu_dt = etap.ddmmyyyy_to_dt(etap.get_udf('Next Pickup Date', account))
+        npu_str = npu_dt.strftime('%A, %B %-d')
+
+        return send_reply(
+            greeting + 'Your next pickup is scheduled on ' + npu_str,
+            resp
+        )
+    # No keyword.
     else:
-        name = request.cookies.get('name')
+        return send_reply(
+            greeting + get_help_reply(resp, account=account),
+            resp
+        )
 
-        if not name:
-            if account['nameFormat'] == 1: # individual
-                name = account['firstName']
-            else:
-                name = account['name']
+#-------------------------------------------------------------------------------
+def new_conversation():
+    if int(request.cookies.get('messagecount', 0)) == 0:
+        return True
+    else:
+        return False
 
-        twml.message("Hi %s. Please reply with keyword SCHEDULE if you'd like to know "\
-            "your next pickup date." % name)
+#-------------------------------------------------------------------------------
+def get_greeting(account=None):
+    '''A simple hello at the beginning of a conversation'''
 
+    greeting = 'Good ' + get_tod()
+
+    if account:
+        if account['nameFormat'] == 1: # individual
+            name = account['firstName']
+        else:
+            name = account['name']
+
+        greeting += ', ' + name + '. '
+    else:
+        greeting += '. '
+
+    return greeting
+
+#-------------------------------------------------------------------------------
+def get_help_reply(resp, account=None):
+    reply = ''
+
+    messagecount = int(request.cookies.get('messagecount', 0))
+
+    if messagecount == 0:
+        reply += 'How can I help you today? '
+
+    reply += 'If you reply with the word "schedule" I can tell you your '\
+             'next pickup date'
+
+    return reply
+
+#-------------------------------------------------------------------------------
+def send_reply(msg, resp):
+    twml = twiml.Response()
+    twml.message(msg)
     resp.data = str(twml)
-
     return resp
+
+#-------------------------------------------------------------------------------
+def get_tod():
+    hour = datetime.now().time().hour
+
+    if hour < 12:
+        return 'morning'
+    elif hour >= 12 and hour < 18:
+        return 'afternoon'
+    elif hour >= 18:
+        return 'evening'
 
 #-------------------------------------------------------------------------------
 def pickup_request(resp, twml):
@@ -142,11 +198,19 @@ def pickup_request(resp, twml):
     return True
 
 #-------------------------------------------------------------------------------
-def identify_user(resp):
+def get_identity(resp):
     agency = db.agencies.find_one({'twilio.sms.number':request.form['To']})
 
+    if request.cookies.get('etap_id'):
+        return etap.call(
+          'get_account',
+          agency['etapestry'],
+          {'account_number':request.cookies['etap_id']},
+          silence_exceptions=True
+        )
+
+    # New conversation. Try to identify phone number
     try:
-        # Look up number in eTap to see if existing account
         account = etap.call(
           'find_account_by_phone',
           agency['etapestry'],

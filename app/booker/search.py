f8eda55985fc53da18d860c4e31b28bb98dfe580
SeanEstey <estese@gmail.com>
2016-11-24 18:13:05 -0700

    booker blueprint

---
+++ app/booker/search.py
@@ -0,0 +1,321 @@
+'''app.booker.search'''
+
+from datetime import datetime, date, timedelta
+from flask_login import current_user
+import logging
+
+from .. import etap, parser
+
+from .. import db
+logger = logging.getLogger(__name__)
+
+#-------------------------------------------------------------------------------
+def search(term, config, agency):
+    '''Search query invoked from Booker client
+    @term: either Account Number, Postal Code, Address, or Block
+    Returns: JSON object: {'search_type': str, 'status': str, 'message': str, 'booking_results': array }
+    '''
+
+    user = db.users.find_one({'user': current_user.username})
+    conf = db.agencies.find_one({user['agency']})
+    map_coords = db.maps.find_one({}) # FIXME
+
+    events = []
+    end_date = date.today() + timedelta(days=30)
+
+    service = gcal.gauth(conf['google']['oauth'])
+
+    for cal_id in conf['cal_ids']:
+        events +=  gcal.get_events(
+            service,
+            conf['cal_ids'][cal_id],
+            date.today(),
+            end_date
+        )
+
+    if parser.is_account_id(term):
+        account_info = get_account_info(term)
+
+        return {
+            'search_type': 'account',
+            'account': account_info,
+            'results': search_by_address(
+                account_info['address'],
+                events,
+                map_coords,
+                conf['booker'],
+                conf['google']['geocode']['api_key'],
+                account = account_info
+            )
+        }
+    elif parser.is_block(term):
+        return {
+            'search_type': 'block',
+            'results': search_by_block(
+                term,
+                events,
+                conf['booker']
+            ),
+            'description': \
+                'Booking suggestions for Block <b>' + term + '</b> '\
+                'within next <b>sixteen weeks</b>'
+        }
+
+    elif parser.is_postal_code(term):
+        return {
+            'search_type': 'postal',
+            'results': search_by_postal(
+                term,
+                events,
+                map_coords,
+                conf['booker'],
+                conf['google']['geocode']['api_key']
+            ),
+            'description': \
+                'Booking suggestions for Postal Code <b>' +\
+                term.substring(0,3) + '</b> within next <b>ten weeks</b>'
+
+        }
+    elif parser.is_address(term):
+        return {
+            'search_type': 'address',
+            'results': search_by_address(
+                term,
+                events,
+                map_coords,
+                conf['booker'],
+                conf['google']['geocode']['api_key']
+            ),
+            'description': \
+                'Booking suggestions for Postal Code <b>' +\
+                term.substring(0,3) + '</b> within next <b>ten weeks</b>'
+        }
+
+    return False
+
+#-------------------------------------------------------------------------------
+def make(account_num, udf, type, config):
+    '''Makes the booking in eTapestry by posting to Bravo.
+    This function is invoked from the booker client.
+    @type: 'delivery, pickup'
+    '''
+
+    '''
+    logger.info('Making ' + type + ' booking for account ' + account_num + ', udf: ' + udf)
+
+    response = Server.call('make_booking', {'account_num':account_num, 'udf':udf, 'type':type}, config['etapestry'])
+
+    logger.info(response.getContentText())
+
+    return response.getContentText()
+    '''
+    return True
+
+#-------------------------------------------------------------------------------
+def get_account_info(acct_id):
+    account_id = term
+
+    if account_id[0] == '/':
+        account_id = account_id[1:]
+
+    logger.info('search by account #')
+
+    try:
+        account = etap.call(
+          'get_account',
+          etapestry_id,
+          {'account_number': account_id}
+        )
+    except Exception as e:
+        logger.error('Error retrieving accounts for query %s', block)
+        response['status'] = 'failed'
+        response['message'] = 'Account <b>' + term + '</b> not found in eTapestry'
+        return False
+
+    response['account_id'] = account['id']
+    response['account_name'] = account['name']
+
+    return {
+        'id': account['id'],
+        'name': account['name']
+    }
+
+    return True
+
+#-------------------------------------------------------------------------------
+def search_by_radius(coords, events, map_data, conf):
+    '''Find a list of Blocks within the smallest radius of given coordinates.
+    Constraints: must be within provided radius, schedule date, and block size.
+    @rules: specifies schedule, radius and block size constraints
+    Returns: list of Block objects on success, [] on failure
+    '''
+
+    '''
+    today = date.today()
+    two_weeks = new Date(today.getTime() + (1000 * 3600 * 24 * rules['max_schedule_days_wait']))
+    radius = 4.0
+
+    found = false
+
+    bookings = []
+
+
+    # Start with small radius, continually expand radius until match found
+    while found == False:
+        if radius > rules['max_block_radius']:
+            break
+
+        bookings = Geo.findBlocksWithin(lat, lng, map_data, radius, two_weeks, cal_ids['res'], _events)
+
+        if len(bookings) > 0:
+            found = true
+        else:
+          logger.info('No match found within ' + radius.toString() + ' km. Expanding search.')
+
+          radius += 1.0
+
+    if found:
+        for i in range(len(booking)):
+            if is_res(bookings[i].block)):
+                bookings[i]['max_size'] = rules['size']['res']['max']
+            else:
+                bookings[i]['max_size'] = rules['size']['bus']['max']
+
+    # May be empty array
+    return bookings
+    '''
+    return True
+
+
+#-------------------------------------------------------------------------------
+def search_by_block(block, events, conf):
+    '''Find all the scheduled dates/info for the given block.
+    @rules: Object specifying search parameters: 'search_weeks' and
+    'size':'res':'max' and 'size:'bus':'max'
+    Returns: list of Block objects on success, [] on failure
+    '''
+
+    '''
+    today = new Date()
+    end_date = new Date(today.getTime() + (1000 * 3600 * 24 * 7 * rules['search_weeks']))
+    res_events = Schedule.getEventsBetween(cal_ids['res'], today, end_date)
+    bus_events = Schedule.getEventsBetween(cal_ids['bus'], today, end_date)
+    events = res_events.concat(bus_events)
+    '''
+
+    results = []
+
+    for event in events:
+        cal_block = parser.get_block(event['summary'])
+
+        if block != cal_block:
+            continue
+
+        '''
+        result = {
+          'block': block_name,
+          'date': parseDate(events[i].start.date),
+          'location': events[i].location,
+          'event_name': events[i].summary.substring(0, events[i].summary.indexOf(']')+1),
+          'booking_size':Parser.getBookingSize(events[i].summary)
+        }
+
+        if(is_res(block_name))
+          result['max_size'] = rules['size']['res']['max']
+        else
+          result['max_size'] = rules['size']['bus']['max']
+        '''
+
+        results.push(result)
+
+    sorted(
+        results,
+        key=lambda event: event['start'].get('dateTime', event['start'].get('date'))
+    )
+
+    return results
+
+#-------------------------------------------------------------------------------
+def search_by_address(address, events, map_coords, conf, api_key, account=None):
+    '''Find results nearby'''
+
+    from app.routing import geo
+
+    geo_results = geo.geocode(address, api_key)
+
+    if len(geo_results) == 0:
+        return {
+            'status': 'failed',
+            'description': \
+                'Could not find local address. '\
+                'Make sure to include quadrant (i.e. NW) and Postal Code.'
+        }
+
+    response['results'] = search_by_radius(
+        geo_results[0].geometry.location,
+        events,
+        map_coords,
+        conf
+    )
+
+    response['message'] = \
+        'Booking suggestions for account <b>' + account['name'] \
+        + '</b> in <b>10km</b> within next <b>14 days</b>'
+
+    return True
+
+#-------------------------------------------------------------------------------
+def search_by_postal(postal, events, map_coords, conf, api_key, account=None):
+    '''Finds all Blocks within given postal code, sorted, sorted by date.
+    Returns: list of Block objects on success, [] on failure.
+    '''
+
+    '''
+    postal = postal.toUpperCase()
+    today = new Date()
+    ten_weeks = new Date(today.getTime() + (1000 * 3600 * 24 * 7 * 10))
+    res_events = Schedule.getEventsBetween(cal_ids['res'], today, ten_weeks)
+    bus_events = Schedule.getEventsBetween(cal_ids['bus'], today, ten_weeks)
+    events = res_events.concat(bus_events)
+
+    results = []
+    for(i=0 i < events.length i++) {
+    event = events[i]
+
+    if(!event.location) {
+      logger.info('Calendar event ' + event.summary + ' missing postal code')
+      continue
+    }
+
+    postal_codes = event.location.split(",")
+
+    for(n=0 n<postal_codes.length n++) {
+      if(postal_codes[n].trim() != postal.substring(0,3))
+        continue
+
+      block = get_block(event.summary)
+      result = {
+        'block': block,
+        'date': parseDate(event.start.date),
+        'event_name': event.summary.substring(0, event.summary.indexOf(']')+1),
+        'location': event.location,
+        'booking_size':Parser.getBookingSize(event.summary)
+      }
+
+      if(is_res(block))
+        result['max_size'] = rules['size']['res']['max']
+      else
+        result['max_size'] = rules['size']['bus']['max']
+
+      results.push(result)
+    }
+    }
+
+    results.sort(function(a, b) {
+    return a.date.getTime() - b.date.getTime()
+    })
+
+    return results
+    }
+    '''
+    return True
---
+++ app/booker/views.py
@@ -0,0 +1,24 @@
+'''booker.views'''
+
+import json
+import twilio.twiml
+import requests
+from datetime import datetime, date, time, timedelta
+from flask import request, jsonify, render_template, redirect, current_app,url_for
+from flask_login import login_required, current_user
+from bson.objectid import ObjectId
+import logging
+
+from . import booker
+from . import search
+from .. import utils
+from .. import db
+logger = logging.getLogger(__name__)
+
+
+#-------------------------------------------------------------------------------
+@booker.route('/', methods=['GET'])
+@login_required
+def show_home():
+    agency = db['users'].find_one({'user': current_user.username})['agency']
+    return render_template('views/booker.html', agency=agency)

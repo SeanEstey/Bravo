6dc8761dd44b0450c13a94a13d049e5a757edfb7
SeanEstey <estese@gmail.com>
2017-02-10 10:53:10 -0700

    ascii bravo startup msg

--- detect.py
+++ detect.py
@@ -3,6 +3,7 @@ import logging, os, time
 import psutil
 from logging import INFO
 from os import environ
+from app.tasks import celery as celery_app
 import celery, eventlet, flask, flask_socketio
 from app.utils import bcolors as c
 from app.logger import file_handler
@@ -13,40 +14,42 @@ def log_startup_info(sio_server, app):
 
     app.logger.info('server starting...\n')
 
-    t1 = c.OKBLUE +  ' _________________'
-    t2 = c.OKBLUE +  '/*****************\\' + c.OKGREEN
-    mp = c.OKBLUE +  '|*****************|' + c.OKGREEN
-    bp = c.OKBLUE +  '\______Ready! ____/' + c.OKGREEN
-
     log_f = c.OKGREEN + '%(message)s' #+ c.ENDC
     hdler = file_handler(INFO, 'info.log', log_f=log_f, log_v='')
     log.addHandler(hdler)
     log.setLevel(INFO)
 
     hostname = environ['BRAVO_HOSTNAME']
-    protocol = 'https://' if environ['BRAVO_SSL'] == 'True' else 'http://'
+    protocol = 'http://' #s://' if environ['BRAVO_SSL'] == 'True' else 'http://'
     host = '%s%s' %(protocol, environ['BRAVO_IP'])
     debug = 'enabled' if app.config['DEBUG'] else 'disabled'
     sandbox = 'enabled' if environ['BRAVO_SANDBOX_MODE'] == 'True' else 'disabled'
-    beat = 'on' if environ['BRAVO_CELERY_BEAT'] == 'True' else 'off'
+    ssl = 'enabled (nginx)' if environ['BRAVO_SSL'] == 'True' else 'disabled'
+
 
     mem = psutil.virtual_memory()
     active = (mem.active/1000000)
     total = (mem.total/1000000)
     free = mem.free/1000000
 
-    log.info(t1)
-    log.info('%s %s%s', t2, c.WARNING, get_os_full_desc())
-    log.info('%s mem free: %s/%s', mp, free, total)
-    log.info('%s %s%sbravo@%s%s' %(mp, c.WARNING, c.BOLD, hostname, c.ENDC))
-    log.info('%s %s' % (mp, host))
-    log.info('%s [config]'% mp)
-    log.info('%s -> debug:   %s' % (mp,debug))
-    log.info('%s -> sandbox: %s' % (mp,sandbox))
-    log.info('%s -> running: flask %s' % (mp, flask.__version__))
-    log.info('%s -> server:  eventlet %s' % (mp, eventlet.__version__))
+    if free < 250:
+        app.logger.info(\
+            '%ssystem has less than 250mb free mem (%smb). '\
+            'not recommended!\n', c.WARNING, free)
 
-    from app.tasks import celery as celery_app
+    log.info(\
+    "%s-------------------------------- %s%s\n"                         %(c.OKGREEN, c.WARNING, get_os_full_desc()) +\
+    "%s-  ____ ------------------------ %smem free: %s/%s\n"            %(c.OKGREEN, c.OKGREEN, free, total) +\
+    "%s- |  _ \ ----------------------- %s%sbravo@%s%s\n"               %(c.OKGREEN, c.WARNING, c.BOLD, hostname, c.OKGREEN) +\
+    "%s- | |_) |_ __ __ ___   _____ --- %s%s\n"                         %(c.OKGREEN, c.OKGREEN, host) +\
+    "%s- |  _ <| '__/ _` \ \ / / _ \ -- %s[config]\n"                   %(c.OKGREEN, c.OKGREEN) +\
+    "%s- | |_) | | | (_| |\ V / (_) | - %s  > debug:   %s\n"          %(c.OKGREEN, c.OKGREEN, debug) +\
+    "%s- |____/|_|  \__,_| \_/ \___/  - %s  > sandbox: %s\n"          %(c.OKGREEN, c.OKGREEN, sandbox) +\
+    "%s-------------------------------- %s  > running: flask %s\n"    %(c.OKGREEN, c.OKGREEN, flask.__version__) +\
+    "%s-------------------------------- %s  > server:  eventlet %s\n" %(c.OKGREEN, c.OKGREEN, eventlet.__version__) +\
+    "%s-------------------------------- %s  > ssl:     %s"            %(c.OKGREEN, c.OKGREEN, ssl) +\
+    "")
+
     inspect = celery_app.control.inspect()
 
     while not inspect.ping():
@@ -54,8 +57,8 @@ def log_startup_info(sio_server, app):
         time.sleep(1)
         inspect = celery_app.control.inspect()
 
-    tasks_reg = '%s registered' % len(inspect.registered().get('celery@bravo'))
-    tasks_sch = '%s scheduled' % len(inspect.scheduled().get('celery@bravo'))
+    tasks_reg = '%s regist.' % len(inspect.registered().get('celery@bravo'))
+    tasks_sch = '%s sched.' % len(inspect.scheduled().get('celery@bravo'))
     stats = inspect.stats()
     celery_host = stats.keys()[0]
     transport = stats[celery_host]['broker']['transport']
@@ -63,15 +66,20 @@ def log_startup_info(sio_server, app):
     broker_location = '%s://%s:%s' %(
         transport, stats[celery_host]['broker']['hostname'],
         stats[celery_host]['broker']['port'])
+    beat = 'on' if environ['BRAVO_CELERY_BEAT'] == 'True' else 'off'
 
-    log.info('%s %scelery@bravo%s' %(mp, c.WARNING, c.ENDC))
-    log.info('%s %s' %(mp, broker_location))
-    log.info('%s [config]' % mp)
-    log.info('%s -> version: %s' % (mp, celery.__version__))
-    log.info('%s -> workers: [%s]' % (mp, worker))
-    log.info('%s -> beat:    %s' % (mp, beat))
-    log.info('%s -> tasks:   %s' % (mp, tasks_reg))
-    log.info('%s -> tasks:   %s\n' % (bp, tasks_sch))
+    log.info(\
+    "%s-------------------------------- %scelery@bravo\n"      %(c.OKGREEN, c.WARNING) +\
+    "%s-------------------------------- %s%s\n"                %(c.OKGREEN, c.OKGREEN, broker_location) +\
+    "%s-------------------------------- %s[config]\n"          %(c.OKGREEN, c.OKGREEN) +\
+    "%s-------------------------------- %s  > version: %s\n"   %(c.OKGREEN, c.OKGREEN, celery.__version__) +\
+    "%s-------------------------------- %s  > workers: [%s]\n" %(c.OKGREEN, c.OKGREEN, worker) +\
+    "%s-------------------------------- %s  > beat:    %s\n"   %(c.OKGREEN, c.OKGREEN, beat) +\
+    "%s-------------------------------- %s  > tasks:   %s\n"   %(c.OKGREEN, c.OKGREEN, tasks_reg) +\
+    "%s-------------------------------- %s  > tasks:   %s\n"     %(c.OKGREEN, c.OKGREEN, tasks_sch) +\
+    "")
+
+    app.logger.info('server ready!')
 
 #-------------------------------------------------------------------------------
 def get_os_full_desc():

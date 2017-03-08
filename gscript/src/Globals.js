f38d18db4b7172271445d2ca1edc9991a866472e
Sean Estey <estese@gmail.com>
2017-03-08 13:41:33 -0700

    Create Globals.js

---
+++ gscript/src/Globals.js
@@ -0,0 +1,25 @@
+API_URL = "https://www.bravoweb.ca/api";
+var DAY_MS = 1000 * 3600 * 24;
+var WEEK_MS = DAY_MS * 7;
+var MONTHS = [
+    "Jan", 
+    "Feb", 
+    "Mar", 
+    "Apr", 
+    "May", 
+    "Jun", 
+    "Jul", 
+    "Aug", 
+    "Sep", 
+    "Oct", 
+    "Nov", 
+    "Dec"
+];
+COLOR = {
+  "light red": "#e06666",
+  "green": "#63d297"
+};
+SYMBOL = {
+  "x": "=CHAR(10006)",
+  "checkmark": "=CHAR(10004)"
+};

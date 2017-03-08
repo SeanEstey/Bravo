d82c2987cdfda18393018dcd37ceff3998f13a1d
Sean Estey <estese@gmail.com>
2017-03-08 13:39:58 -0700

    Update Route.js

--- gscript/src/Route.js
+++ gscript/src/Route.js
@@ -1,163 +1,124 @@
+function Route(){}
+
 //---------------------------------------------------------------------
-function Route(id) {
-  this.id = id;
-  this.ss = SpreadsheetApp.openById(id);
-  this.sheet = this.ss.getSheets()[0];
+function Route(agcy, ss_id) {
+  /* Object containing data and properties from a route SS
+
+  */
   
-  var data = this.sheet.getDataRange().getValues();
-  
-  this.headers = data.slice(0,1)[0];
-  
-  var num_orders = this.sheet.getRange("E1:E")
-    .getValues().filter(String).length - 1;
-  
-  // Order rows excluding last Depot row
-  this.orders = data.slice(1, num_orders + 1);
-  
-  // Route title format: "Dec 27: R4E (Ryan)"
-  this.title = this.ss.getName();
-  this.title_block = Parser.getBlockFromTitle(this.title);
-     
-  var date_str = this.title.substring(0, this.title.indexOf(":"));
-  var full_date_str = date_str + ", " + (new Date()).getFullYear();
-  this.date = new Date(full_date_str);
-  this.driver = this.title.substring(
-    this.title.indexOf("(")+1, 
-    this.title.indexOf(")")
-  );
-   
-//  Logger.log('New Route block: ' + this.title_block);
-    
-  this.months = [
-    "Jan", 
-    "Feb", 
-    "Mar", 
-    "Apr", 
-    "May", 
-    "Jun", 
-    "Jul", 
-    "Aug", 
-    "Sep", 
-    "Oct", 
-    "Nov", 
-    "Dec"
-  ];
+  this.agcy = agcy;
+  this.ss_id = ss_id;
+  this.ss = SpreadsheetApp.openById(ss_id);
+  this.getOrders();
+  this.getInfoProperties();
+  this.getInventory();
 }
 
 //---------------------------------------------------------------------
-Route.prototype.getValue = function(order_idx, column_name) {
-  return this.orders[order_idx][this.headers.indexOf(column_name)] || '';
+Route.prototype.getOrders = function() {
+  /* this.orders: 2D array of values. Excludes Depot, Office stops
+  */
+  
+  var orders_wks = this.ss.getSheetByName("Orders");
+  var values = orders_wks.getDataRange().getValues();
+  
+  this.orders_hdr = values[0];
+  this.orders = values.filter(getIdFilter(values[0].indexOf('ID')));
+}
+
+//---------------------------------------------------------------------
+Route.prototype.getInfoProperties = function() {
+  
+  var info_wks = this.ss.getSheetByName("Info");
+  
+  var values = info_wks.getRange(
+    2, 
+    1,
+    info_wks.getMaxRows(),
+    info_wks.getMaxColumns())
+  .getValues();
+  
+  this.properties = {};
+  
+  for(var i=0; i<values.length; i++) {
+    // TODO: what about duplicate values from multiple drivers?
+    this.properties[values[i][0]] = values[i][1];
+  }
+}
+
+//---------------------------------------------------------------------
+Route.prototype.getInventory = function() {
+  
+  var inven_wks = this.ss.getSheetByName("Inventory");
+  
+  if(inven_wks == null)
+    return;
+  
+  var values = inven_wks.getRange(
+    2, 
+    1,
+    inven_wks.getMaxRows(),
+    inven_wks.getMaxColumns())
+  .getValues();
+  
+  this.inventory = {};
+  
+  for(var i=0; i<values.length; i++) {
+    if(values[i][0].search("Select") == -1)
+      this.inventory[values[i][0]] = values[i][1];
+  }
 }
 
 //---------------------------------------------------------------------
 Route.prototype.orderToDict = function(idx) {
-  /* For RouteProcessor processing */
+  /* Converts row array from route into key/value dictionary
+   */
   
   if(idx >= this.orders.length)
     return false;
   
-  var order_info = this.getValue(idx,'Order Info');
+  var order_info = this.getOrderValue(idx,'Order Info');
   var act_name_regex = /Name\:\s(([a-zA-Z]*?\s)*){1,5}/g;
   var account_name = '';
       
   // Parse Account Name from "Order Info" string
   if(act_name_regex.test(order_info))
-    account_name = order_info.match(act_name_regex)[0] + '\n';    
+    account_name = order_info.match(act_name_regex)[0];   
+  
+  var gift = false;
+  if(isNumber(this.orders[idx][this.orders_hdr.indexOf('$')]))
+     gift = Number(this.orders[idx][this.orders_hdr.indexOf('$')]);
     
   return {
-    'Account Number': this.getValue(idx,'ID'),
-    'Name & Address': account_name + this.getValue(idx,'Address'),
-    'Gift Estimate': Number(this.getValue(idx,'$')),
-    'Driver Input': this.getValue(idx,'Notes'),
-    'Driver Notes': this.getValue(idx,'Driver Notes'),
-    'Block': this.getValue(idx,'Block').replace(/, /g, ','),
-    'Neighborhood': this.getValue(idx,'Neighborhood').replace(/, /g, ','),
-    'Status': this.getValue(idx,'Status'),
-    'Office Notes': this.getValue(idx,'Office Notes')
+    'ID': this.getOrderValue(idx,'ID'),
+    'Address': this.getOrderValue(idx, 'Address') || '',
+    'Account': account_name + this.getOrderValue(idx,'Address'),
+    'Estimate': gift,
+    'Notes': (this.getOrderValue(idx,'Notes') || ''),
+    'Driver Notes': (this.getOrderValue(idx,'Driver Notes') || ''),
+    'Block': this.getOrderValue(idx,'Block').replace(/, /g, ','),
+    'Neighborhood': (this.getOrderValue(idx,'Neighborhood') || '').replace(/, /g, ','),
+    'Status': this.getOrderValue(idx,'Status'),
+    'Office Notes': (this.getOrderValue(idx,'Office Notes') || '')
   }
 }
 
 //---------------------------------------------------------------------
-Route.prototype.getInfo = function() { 
-  /* Gather Stats and Inven fields from bottom section of Route, build 
-   * dictionary
-   * Returns: Dict object on success, false on error
-   */
-  
-  var a = this.sheet.getRange(
-    this.orders.length+3,
-    1,
-    this.sheet.getMaxRows()-this.orders.length+1,
-    1).getValues();
-
-  // Make into 1D array of field names: ["Total", "Participants", ...]
-  a = a.join('//').split('//');
-   
-  var start = a.indexOf('***Route Info***');
-  
-  if(start < 0)
-    return false;
-  else
-    start++;
-  
-  a.splice(0, start);
-  
-  var inven_idx = a.indexOf('***Inventory***');
-  
-  if(inven_idx < 0)
-    return false;
-  
-  // Now left with Stats and Inventory field names
-  
-  stats_fields = a.splice(0, inven_idx);
-  
-  var stats = {};
-  
-  var b = this.sheet.getRange(
-    this.orders.length+3,
-    2,
-    this.sheet.getMaxRows()-this.orders.length+1,
-    1).getValues();
-  
-  b.splice(0, start);
-  
-  for(var i=0; i<stats_fields.length; i++) {
-    var key = stats_fields[i]; 
-    stats[key] = b[i][0];
-  }
-  
-  return stats;
+Route.prototype.getBlockType = function() {
+  if(Parser.isRes(this.properties['Block']))
+     return 'Res';
+  else if(Parser.isBus(this.properties['Block']))
+    return 'Bus';
 }
 
 //---------------------------------------------------------------------
-Route.prototype.getInventoryChanges = function() {
-  var a = this.sheet.getRange(1,1,this.sheet.getMaxRows(),1).getValues();
-  var b = this.sheet.getRange(1,2,this.sheet.getMaxRows(),1).getValues();
-  
-  a = a.join('//').split('//');
-  b = b.join('//').split('//');
-  
-  var inven_idx = a.indexOf('***Inventory***');
-  
-  if(inven_idx < 0)
-    return false;
-  
-  a = a.slice(inven_idx + 1, a.length);
-  b = b.slice(inven_idx + 1, b.length);
+Route.prototype.getOrderValue = function(order_idx, column_name) {
+  return this.orders[order_idx][this.orders_hdr.indexOf(column_name)] || false;
+}
 
-  /* TODO: Loop through spliced array, make dictionary of all fields and values without referencing them by name below */
-  
-  return {
-    'Bag Buddies In': b[a.indexOf('Bag Buddies In')],
-    'Bag Buddies Out': b[a.indexOf('Bag Buddies Out')],
-    'Green Bags': b[a.indexOf('Green Bags')],
-    'Green Logo Bags': b[a.indexOf('Green Logo Bags')],
-    'White Bags': b[a.indexOf('White Bags')],
-    'Green Bins In': b[a.indexOf('Green Bins In')],
-    'Green Bins Out': b[a.indexOf('Green Bins Out')],
-    'Blue Bins In': b[a.indexOf('Blue Bins In')],
-    'Blue Bins Out': b[a.indexOf('Blue Bins Out')],
-    'Bottle Bins In': b[a.indexOf('Bottle Bins In')],
-    'Bottle Bins Out': b[a.indexOf('Bottle Bins Out')]
-  };
+//---------------------------------------------------------------------
+function getIdFilter(col_idx) {  
+  return function(element) {
+    return isNumber(element[col_idx]);
+  }
 }

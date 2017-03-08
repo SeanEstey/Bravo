187b0088d00e31f852a72b9b02326fc6b4eb0ead
SeanEstey <estese@gmail.com>
2017-03-08 13:38:52 -0700

    setup

--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
 import os
 
 if __name__ == "__main__":
-    os.system('mkdir /var/www/bravo')
-    os.system('mkdir /var/www/bravo/logs')
-    os.system('chown -R www-data:root /var/www/bravo/logs')
+    os.system('mkdir logs')
+    os.system('chown -R www-data:root logs')
     os.system('cp virtual_host/default /etc/nginx/sites-enabled/')
     os.system('service nginx restart')
     os.system('cp logrotate/bravo /etc/logrotate.d/')

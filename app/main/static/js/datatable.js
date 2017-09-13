9c3978642b4b32b7e7d448a3abbbeb4ceb8cc1ed
SeanEstey <estese@gmail.com>
2017-09-12 21:56:49 -0600

    added db upgrade module

--- app/main/static/js/datatable.js
+++ app/main/static/js/datatable.js
@@ -2,168 +2,50 @@
 v0.11
 */
 
-fields = [
-    {
-        column:     { title:'Timestamp' },
-        data:       { k:'date', sub_k:'$date' },
-        columnDef:  { targets:0, visible:false, searchable:false }
-    },
-    {
-        column:     { title:'Date'+
-                            '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;',
-                      width:"15%" },
-        data:       { k:'date', sub_k:'$date', value:function(v){ return new Date(v).strftime('%b %d %Y') } },
-        columnDef:  { targets:1, width:"15%" }
-    },
-    {
-        column:     { title:'Block' },
-        data:       { k:'block', sub_k:false },
-        columnDef:  false
-    },
-    {
-        column:     { title:'Size' },
-        data:       { k:'stats', sub_k:'nBlockAccounts' },
-        columnDef:  false
-    },
-    {
-        column:     { title:'Skips' },
-        data:       { k:'stats', sub_k:'nSkips' },
-        columnDef:  false
-    },
-    {
-        column:     { title:'Orders' },
-        data:       { k:'stats', sub_k:'nOrders' },
-        columnDef:  false
-    },
-    {
-        column:     { title:'Zeros' },
-        data:       { k:'stats', sub_k:'nZeros' },
-        columnDef:  false
-    },
-    {
-        column:     { title:'Donations' },
-        data:       { k:'stats', sub_k:'nDonations' },
-        columnDef:  false
-    },
-    {
-        column:     { title:'Collect. Rate' },
-        data:       { k:'stats', sub_k:'collectionRate',
-                      value:function(v){ return typeof(v)=='number' ? Sugar.Number.format(v*100,1)+'%' : '' } },
-        columnDef:  false
-    },
-    {
-        column:     { title:'Estimate' },
-        data:       { k:'stats', sub_k:'estimateTotal' },
-        columnDef:  { targets:9, render:function(data,type,row){ return data ? '$'+Sugar.Number.format(data,2) : ''; } }
-    },
-    {
-        column:     { title:'Receipt' },
-        data:       { k:'stats', sub_k:'receiptTotal' },
-        columnDef:  { targets:10, render:function(data,type,row){ return data ? '$'+Sugar.Number.format(data,2) : ''; } }
-    },
-    {
-        column:     { title:'Estimate Avg' },
-        data:       { k:'stats', sub_k:'estimateAvg' },
-        columnDef:  { targets:11, render:function(data,type,row){ return data ? '$'+Sugar.Number.format(data,2) : ''; } }
-    },
-    {
-        column:     { title:'Estimate Trend' },
-        data:       { k:'stats', sub_k:'estimateTrend',
-                      value:function(v){ return '$'+Sugar.Number.format(v,2); } },
-        columnDef:  false
-    },
-    {
-        column:     { title:'Estimate Margin' },
-        data:       { k:'stats', sub_k:'estimateMargin',
-                      value:function(v){ return typeof(v)=='number' ? Sugar.Number.format(v*100,1)+'%' : '' } },
-        columnDef:  false
-    },
-    {
-        column:     { title:'Status' },
-        data:       { k:'routific', sub_k:'status'},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Unserved' },
-        data:       { k:'routific', sub_k:'nUnserved'},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Warnings' },
-        data:       { k:'routific', sub_k:'warnings', value:function(v){ return v.length}},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Errors' },
-        data:       { k:'routific', sub_k:'errors', value:function(v){return v.length}},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Depot' },
-        data:       { k:'routific', sub_k:'depot', value:function(v){ return v.name? v.name : '' }},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Driver' },
-        data:       { k:'routific', sub_k:'driver', value:function(v){ return v.name? v.name : '' }},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Invoice' },
-        data:       { k:'driverInput', sub_k:'invoiceNumber'},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Mileage' },
-        data:       { k:'driverInput', sub_k:'mileage'},
-        columnDef:  false
-    },
-    {
-        column:     { title:'RA' },
-        data:       { k:'driverInput', sub_k:'raName'},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Vehicle' },
-        data:       { k:'driverInput', sub_k:'vehicle' },
-        columnDef:  false
-    },
-    {
-        column:     { title:'RA Hrs' },
-        data:       { k:'driverInput', sub_k:'raHrs'},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Driver Hrs' },
-        data:       { k:'driverInput', sub_k:'driverHrs'},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Vehicle Inspection' },
-        data:       { k:'driverInput', sub_k:'vehicleInspection'},
-        columnDef:  { targets:26, visible:false }
-    },
-    {
-        column:     { title:'Notes' },
-        data:       { k:'driverInput', sub_k:'notes'},
-        columnDef:  { targets:27, visible:false }
-    },
-    {
-        column:     { title:'Cages' },
-        data:       { k:'driverInput', sub_k:'nCages'},
-        columnDef:  false
-    },
-    {
-        column:     { title:'Total Duration' },
-        data:       { k:'routific', sub_k:'totalDuration', value:function(x){ return Sugar.Number.format(x/60,2) } },
-        columnDef:  false
-    }
-];
+num_format = Sugar.Number.format;
 
+data_tag = 'routes_new';
 tbl_id = 'datatable';
 tbl_data = [];
-data_tag = 'routes_new';
 datatable = null;
+fields = [
+    { column:{ title:'Timetamp' }, columnDef:{ targets:0, visible:false, searchable:false }, data:{ k:'date', sub_k:'$date' } },
+    { column:{ title:'Date&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;', width:"15%" },
+      columnDef:{ targets:1, width:"15%" }, data:{ k:'date', sub_k:'$date', value:function(v){ return new Date(v).strftime('%b %d %Y') } } },
+    { column:{ title:'Block' }, columnDef:false, data:{ k:'block', sub_k:false } },
+    { column:{ title:'Size' }, columnDef:false, data:{ k:'stats', sub_k:'nBlockAccounts' } },
+    { column:{ title:'Skips' }, columnDef:false, data:{ k:'stats', sub_k:'nSkips' } },
+    { column:{ title:'Orders' }, columnDef:false, data:{ k:'stats', sub_k:'nOrders' } },
+    { column:{ title:'Zeros' }, columnDef:false, data:{ k:'stats', sub_k:'nZeros' } },
+    { column:{ title:'Donations' }, columnDef:false, data:{ k:'stats', sub_k:'nDonations' } },
+    { column:{ title:'Collect. Rate' }, columnDef:false, 
+      data:{ k:'stats', sub_k:'collectionRate', value:function(v){ return typeof(v)=='number'? num_format(v*100,1)+'%':'' } } },
+    { column:{ title:'Estimate' }, data:{ k:'stats', sub_k:'estimateTotal' },
+      columnDef:{ targets:9, render:function(data, type, row){ return data? '$'+num_format(data,2) :'' } } },
+    { column:{ title:'Receipt' }, data:{ k:'stats', sub_k:'receiptTotal' },
+      columnDef:{ targets:10, render:function(data, type, row){ return data? '$'+num_format(data,2) :'' } } },
+    { column:{ title:'Estimate Avg' }, data:{ k:'stats', sub_k:'estimateAvg' },
+      columnDef:{ targets:11, render:function(data,type,row){ return data? '$'+num_format(data,2) :'' } } },
+    { column:{ title:'Estimate Trend' }, columnDef:false, data:{ k:'stats', sub_k:'estimateTrend', value:function(v){ return '$'+num_format(v,2) } } },
+    { column:{ title:'Estimate Margin' }, columnDef:false,
+      data:{ k:'stats', sub_k:'estimateMargin', value:function(v){ return typeof(v)=='number'? num_format(v*100,1)+'%' :'' } } },
+    { column:{ title:'Status' }, columnDef:false, data:{ k:'routific', sub_k:'status'} },
+    { column:{ title:'Unserved' }, columnDef:false, data:{ k:'routific', sub_k:'nUnserved'} },
+    { column:{ title:'Warnings' }, columnDef:false, data:{ k:'routific', sub_k:'warnings', value:function(v){ return v.length }} },
+    { column:{ title:'Errors' }, columnDef:false, data:{ k:'routific', sub_k:'errors', value:function(v){ return v.length } } },
+    { column:{ title:'Depot' }, columnDef:false, data:{ k:'routific', sub_k:'depot', value:function(v){ return v.name? v.name : ''} } },
+    { column:{ title:'Driver' }, columnDef:false, data:{ k:'routific', sub_k:'driver', value:function(v){ return v.name? v.name : ''} } },
+    { column:{ title:'Invoice' }, columnDef:false, data:{ k:'driverInput', sub_k:'invoiceNumber'} },
+    { column:{ title:'Mileage' }, columnDef:false, data:{ k:'driverInput', sub_k:'mileage'} },
+    { column:{ title:'RA' }, columnDef:false, data:{ k:'driverInput', sub_k:'raName'}, },
+    { column:{ title:'Vehicle' }, columnDef:false, data:{ k:'driverInput', sub_k:'vehicle' } },
+    { column:{ title:'RA Hrs' }, columnDef:false, data:{ k:'driverInput', sub_k:'raHrs'} },
+    { column:{ title:'Driver Hrs' }, columnDef:false, data:{ k:'driverInput', sub_k:'driverHrs'} },
+    { column:{ title:'Vehicle Inspection' }, columnDef:{ targets:26, visible:false }, data:{ k:'driverInput', sub_k:'vehicleInspection'} },
+    { column:{ title:'Notes' }, columnDef:{ targets:27, visible:false }, data:{ k:'driverInput', sub_k:'notes'} },
+    { column:{ title:'Cages' }, columnDef:false, data:{ k:'driverInput', sub_k:'nCages'} },
+    { column:{ title:'Total Duration' }, columnDef:false, data:{ k:'routific', sub_k:'totalDuration', value:function(x){ return num_format(x/60,2) } } }
+];
 
 //------------------------------------------------------------------------------
 function initDatatable() {
@@ -240,109 +122,5 @@ function formatDataNew(data) {
         tbl_data.push(tbl_row);
     }
 
-    //console.log(tbl_data);
     return tbl_data;
-}
-
-
-
-//------------------------------------------------------------------------------
-function formatDataOld(resp_data) {
-
-    var data_hdr = [
-        "date", "block", "skips", "orders", "zeros", "donors", "estmt", "receipt",
-        "collectRate", "estmtMargin", "estmtTrend", "invoice", "tripSched", "mileage",
-        "vehicle", "driver", "tripActual", "driverHrs", "cages"
-    ];
-    var tbl_rows = [];
-    var tbl_columns = [{title:'timestamp'}];
-    for(var i=0; i<data_hdr.length; i++) {
-        tbl_columns.push({title:Sugar.String.titleize(data_hdr[i])});
-    }
-
-    // Widen Date column w/ extra space
-    tbl_columns[1] = {title:'Date&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'};
-
-    for(var i=0; i<resp_data.length; i++) {
-        var fields = resp_data[i];
-        var tbl_row = [fields['date']['$date']];
-        for(var j=0; j<data_hdr.length; j++) {
-            tbl_row.push(fields[data_hdr[j]] || ''); 
-        }
-        tbl_rows.push(tbl_row);
-    }
-
-    return tbl_rows;
-}
-
-
-// New route format
-NEW_FORMAT= {
-    "_id" : "ObjectId(59b6c77e4da93a19652ee135)",
-    "date" : {"$date":1505168551187},
-    "block" : "R2F",
-    "group" : "wsf",
-    "stats" : {
-        "nOrders" : 68,
-        "receiptTotal" : null,
-        "estimateAvg" : null,
-        "estimateTotal" : null,
-        "collectionRate" : null,
-        "receiptAvg" : null,
-        "nDropoffs" : 0,
-        "nBlockAccounts" : 83,
-        "estimateTrend" : null,
-        "nZeros" : null,
-        "nDonations" : null,
-        "nSkips" : null,
-        "estimateMargin" : null
-    },
-    "routific" : {
-        "status" : "pending",
-        "nUnserved" : null,
-        "nOrders" : null,
-        "warnings" : [],
-        "driver" : {
-            "shift_start" : "08:00",
-            "name" : "Default"
-        },
-        "startAddress" : null,
-        "postal" : "",
-        "orders" : [],
-        "errors" : [],
-        "endAddress" : null,
-        "jobID" : null,
-        "travelDuration" : null,
-        "depot" : {
-            "blocks" : [ 
-                "R2L", 
-                "R10M", 
-                "R1i", 
-                "R1K", 
-                "R2N", 
-                "R3G", 
-                "R4M", 
-                "R5F", 
-                "R5C", 
-                "R7M", 
-                "R8M", 
-                "R9G"
-            ],
-            "formatted_address" : "10347 73 Ave NW, Edmonton, AB",
-            "name" : "Strathcona"
-        },
-        "totalDuration" : null
-    },
-    "driverInput" : {
-        "invoiceNumber" : null,
-        "mileage" : null,
-        "raName" : null,
-        "driverName" : null,
-        "vehicle" : null,
-        "raHrs" : null,
-        "driverHrs" : null,
-        "vehicleInspection" : null,
-        "notes" : null,
-        "nCages" : null
-    }
 }
